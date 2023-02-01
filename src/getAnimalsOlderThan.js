bfd6cdc3baedc0528aa8d61ba3071facc4aba404
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-01 15:45:44 -0300

    Cumprindo requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animalSpecies = data.species;
+  const individuals = animalSpecies.find((item) => item.name === animal).residents;
+  return individuals.every((item) => item.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
