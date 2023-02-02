6981c9dc37e34c401e2e45c78c68ce9b03af7eb5
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-02 18:15:43 -0300

    Cumprindo requisito 6. Pulando o 5 para tentar depois

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  
+const getAnimal = (animal) => {
+  const animals = data.species;
+  const selectedAnimal = animals.find((item) => item.name === animal);
+  const obj = {
+    name: selectedAnimal.name,
+    count: selectedAnimal.residents.length,
+  };
+
+  return obj;
 };
+const countAnimals = (animal) => {
+  const currentAnimal = getAnimal(animal);
+  return currentAnimal;
+};
+console.log(countAnimals('tigers'));
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,18 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Testa caso de passar o argumento count', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  test('Testa se o resultado é um array', () => {
+    expect(typeof handlerElephants('names')).toBe('object');
+  });
+
+  test('Testa caso de passar o argmento names', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+
+  test('Testa o caso das médias das idades dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+});
