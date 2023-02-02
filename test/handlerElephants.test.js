89e96ed0cf88f634e841e334397a22288c6eaa2d
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-02 18:47:00 -0300

    Cumprindo requisito 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -4,9 +4,6 @@ describe('Testes da função HandlerElephants', () => {
   test('Testa caso de passar o argumento count', () => {
     expect(handlerElephants('count')).toBe(4);
   });
-  test('Testa se o resultado é um array', () => {
-    expect(typeof handlerElephants('names')).toBe('object');
-  });
 
   test('Testa caso de passar o argmento names', () => {
     expect(handlerElephants('names')).toContain('Jefferson');
@@ -22,5 +19,13 @@ describe('Testes da função HandlerElephants', () => {
 
   test('Testa se caso o valor passado como parâmetro não seja string, retorna uma mensagem de erro', () => {
     expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  test('Verifica se caso o valor passado como parâmetro não exista, retorna null', () => {
+    expect(handlerElephants('address')).toBe(null);
+  });
+
+  test('Verifica se caso receba como parâmetro, qualquer chave do objeto Elephants, retorna o valor dessa chave', () => {
+    expect(handlerElephants('name')).toBe('elephants');
   });
 });
