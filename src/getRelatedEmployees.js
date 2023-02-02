037544a94ae54e9029a017add5c395051c32c4ec
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-02 15:37:16 -0300

    Cumprindo requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,7 +8,18 @@ const isManager = (id) => {
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const employeesList = data.employees;
+  const managedEmployees = employeesList
+    .filter((employee) => employee.managers.includes(managerId));
+
+  const result = managedEmployees
+    .map((item) => `${item.firstName} ${item.lastName}`);
+  return result;
 };
+
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
