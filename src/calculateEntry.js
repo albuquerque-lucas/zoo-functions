a23212725c6f15a39e55cc4f0d47ad18ace57d6f
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-03 14:31:32 -0300

    Realizando função countEntrants

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
+const testEntrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const childrenCount = entrants.filter((item) => item.age < 18).length;
+	const adultCount = entrants.filter((item) => item.age >= 18 && item.age < 50).length;
+	const seniorCount = entrants.filter((item) => item.age >= 50).length;
+  return { child: childrenCount, adult: adultCount, senior: seniorCount };
 };
 
 const calculateEntry = (entrants) => {
   // seu código aqui
 };
+
+console.log(countEntrants(testEntrants));
 
 module.exports = { calculateEntry, countEntrants };
