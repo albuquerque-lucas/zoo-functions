961bd385648c5073e8ff6dd6031f79bc40585906
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-06 13:22:33 -0300

    Cumprindo requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const workers = data.employees;
+const animals = data.species;
+
+const getProperty = (id, property) => {
+  const currentAnimalData = animals.find((item) => item.id === id)[property];
+  return currentAnimalData;
+};
+
+const getWorkersList = () => {
+  const workersList = workers.map((item) => {
+    const obj = {
+      id: item.id,
+      fullName: `${item.firstName} ${item.lastName}`,
+      species: item.responsibleFor.map((element) => getProperty(element, 'name')),
+      locations: item.responsibleFor.map((element) => getProperty(element, 'location')),
+    };
+    return obj;
+  });
+  return workersList;
+};
+
+const getEmployeesCoverage = (person) => {
+  const workerList = getWorkersList();
+  if (!person) return workerList;
+  const selectedPerson = workerList
+    .find((item) => item.fullName.includes(person.name) || item.id === person.id);
+  if (selectedPerson === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return selectedPerson;
 };
 
 module.exports = getEmployeesCoverage;
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,16 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  test('Testa se caso nenhum argumento seja passado, retorna o array de horários', () => {
+    const hoursObjs = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(getOpeningHours()).toEqual(hoursObjs);
+  });
+});
