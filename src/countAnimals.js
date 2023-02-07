bd9ca90ebcccb25360304d23591541569083aa51
albuquerque-lucas <lucaslpra@gmail.com>
2023-02-07 15:33:50 -0300

    Cumprindo requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,19 +1,49 @@
 const data = require('../data/zoo_data');
 
-const getAnimal = (animal) => {
-  const animals = data.species;
-  const selectedAnimal = animals.find((item) => item.name === animal);
-  const obj = {
-    name: selectedAnimal.name,
-    count: selectedAnimal.residents.length,
-  };
+const animals = data.species;
 
-  return obj;
+const getAnimalBySex = (selected, name, animal) => {
+  const selectedSexAnimalCount = selected.residents
+    .filter((item) => item.sex === animal.sex)
+    .length;
+  return selectedSexAnimalCount;
 };
+
 const countAnimals = (animal) => {
-  const currentAnimal = getAnimal(animal);
-  return currentAnimal;
+  if (!animal) {
+    const animalObj = {};
+    animals.forEach((item) => {
+      animalObj[item.name] = item.residents.length;
+    });
+    return animalObj;
+  }
+  const selectedAnimal = animals.find((item) => item.name === animal.species);
+  const animalName = selectedAnimal.name;
+  if (animal.sex) {
+    return getAnimalBySex(selectedAnimal, animalName, animal);
+  }
+  if (animal.species && animal.sex === undefined) {
+    const residentsCount = selectedAnimal.residents.length;
+    return residentsCount;
+  }
 };
-console.log(countAnimals('tigers'));
+
+console.log(countAnimals({ species: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
+
+// const animalNameList = { ...animals.map((item) => item.name) };
+// const revertedArray = Object.entries(animalNameList).map(([key, value]) => [value, key]);
+// const inverseNameList = Object.fromEntries(revertedArray);
+// const inversedKeys = Object.keys(inverseNameList);
+// const animalCountingList = animals.map((item) => item.residents.length);
+// for (let i = 0; i < animalCountingList.length; i += 1) {
+//   let obj = {
+//     [inversedKeys[i]]: animalCountingList[i],
+//   };
+// }
+// let i = 0;
+// for (const key in inverseNameList) {
+//   inverseNameList[key] = animalCountingList[i];
+//   i += 1;
+// }
